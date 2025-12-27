117f3be8cb62dbffd36aaa2ea1c1d36fb4d7a653
Balaji <poralabalaji@gmail.com>
2025-12-27 15:24:41 +0530

    prediction button update

--- app.py
+++ app.py
@@ -6,57 +6,67 @@ import pandas as pd
 import pickle
 
 # load the model
-model=tf.keras.models.load_model("model.h5")
+model = tf.keras.models.load_model("model.h5")
 
 # load the encoders and scaler
-with open("label_encoder_gender.pkl","rb") as file:
-    label_encoder_gender=pickle.load(file)
+with open("label_encoder_gender.pkl", "rb") as file:
+    label_encoder_gender = pickle.load(file)
 
-with open("one_hot_encoder.pkl","rb") as file:
-    label_encoder_geo=pickle.load(file)
+with open("one_hot_encoder.pkl", "rb") as file:
+    label_encoder_geo = pickle.load(file)
 
-with open("scaler.pkl","rb") as file:
-    scaler=pickle.load(file)
+with open("scaler.pkl", "rb") as file:
+    scaler = pickle.load(file)
 
 ## streamlit app
-st.title("Customer Churn Prediction")
+st.title("Customer Churn Prediction App")
 
 ## user input
-geography=st.selectbox("Gepgraphy",label_encoder_geo.categories_[0])
-gender=st.selectbox("Gender",label_encoder_gender.classes_)
-age=st.slider("Age",18,92)
-balance=st.number_input("Balance")
-credit_score=st.number_input("Credit Score")
-estimated_salary=st.number_input("Estimated Salary")
-tenure=st.slider("Tenure",0,10)
-num_of_products=st.slider("Number of Products",1,4)
-has_cr_card=st.selectbox("Has Credit Card",[0,1])
-is_active_number=st.selectbox("Is Active Member",[0,1])
+geography = st.selectbox("Geography", label_encoder_geo.categories_[0])
+gender = st.selectbox("Gender", label_encoder_gender.classes_)
+age = st.slider("Age", 18, 92)
+balance = st.number_input("Balance")
+credit_score = st.number_input("Credit Score")
+estimated_salary = st.number_input("Estimated Salary")
+tenure = st.slider("Tenure", 0, 10)
+num_of_products = st.slider("Number of Products", 1, 4)
+has_cr_card = st.selectbox("Has Credit Card", [0, 1])
+is_active_member = st.selectbox("Is Active Member", [0, 1])
 
-## prepare the input data
-input_data=pd.DataFrame({
-    "CreditScore":[credit_score],
-    "Geography":[geography],
-    "Gender":[label_encoder_gender.transform([gender])[0]],
-    "Age":[age],
-    "Tenure":[tenure],
-    "Balance":[balance],
-    "NumOfProducts":[num_of_products],
-    "HasCrCard":[has_cr_card],
-    "IsActiveMember":[is_active_number],
-    "EstimatedSalary":[estimated_salary]
-})
+# Predict Button
+if st.button("Predict Churn"):
 
-geo_encoded=label_encoder_geo.transform([input_data["Geography"]]).toarray()
-geo_encoded_df=pd.DataFrame(geo_encoded,columns=label_encoder_geo.get_feature_names_out(["Geography"]))
-input_data=pd.concat([input_data.drop("Geography",axis=1),geo_encoded_df],axis=1)
+    ## prepare input data
+    input_data = pd.DataFrame({
+        "CreditScore": [credit_score],
+        "Geography": [geography],
+        "Gender": [label_encoder_gender.transform([gender])[0]],
+        "Age": [age],
+        "Tenure": [tenure],
+        "Balance": [balance],
+        "NumOfProducts": [num_of_products],
+        "HasCrCard": [has_cr_card],
+        "IsActiveMember": [is_active_member],
+        "EstimatedSalary": [estimated_salary]
+    })
 
-input_data_scaled=scaler.transform(input_data)
+    # One-hot encoding for Geography
+    geo_encoded = label_encoder_geo.transform(input_data[["Geography"]]).toarray()
+    geo_encoded_df = pd.DataFrame(geo_encoded,
+                                  columns=label_encoder_geo.get_feature_names_out(["Geography"]))
+    input_data = pd.concat([input_data.drop("Geography", axis=1), geo_encoded_df], axis=1)
 
-prediction=model.predict(input_data_scaled)
-prediction_proba=prediction[0][0]
-st.write("Churn Probability:",prediction_proba)
-if prediction_proba>0.5:
-    st.write("The customer is likely to churn.")
-else:
-    st.write("The customer is not likely to churn.")
+    # Scaling
+    input_data_scaled = scaler.transform(input_data)
+
+    # Prediction
+    prediction = model.predict(input_data_scaled)
+    prediction_proba = prediction[0][0]
+
+    st.subheader("Prediction Result:")
+    st.write(f"Churn Probability: **{prediction_proba:.2f}**")
+
+    if prediction_proba > 0.5:
+        st.error("The customer is likely to churn.")
+    else:
+        st.success("The customer is not likely to churn.")
